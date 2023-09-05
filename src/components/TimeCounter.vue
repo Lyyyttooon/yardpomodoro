fbc89ea6e0f11401a284f657291b249a1b249ad6
Lyyyttooon <84236702+Lyyyttooon@users.noreply.github.com>
2023-09-05 21:42:41 +0800

    feat: 增加计时器

---
+++ src/components/TimeCounter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const timeCount = ref(0)
+
+setInterval(() => {
+  timeCount.value++
+}, 1000);
+</script>
+
+<template>
+  <div>{{ timeCount }}</div>
+</template>

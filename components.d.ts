c0a0faa1245a3cf757ed3f480a8cd1456ee0de5e
Lyyyttooon <84236702+Lyyyttooon@users.noreply.github.com>
2023-09-08 22:25:52 +0800

    feat: 增加设置时间限定范围&增加专注完成弹窗

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
